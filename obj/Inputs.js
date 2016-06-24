c2b7316a7a560a9a416a4aafd97d69f6900446d3
Ewan Breakey <ebreakey@gmail.com>
2016-06-24 20:58:24 +1000

    Can Restart on End GUI (Closes #8)

--- obj/Inputs.js
+++ obj/Inputs.js
@@ -3,5 +3,6 @@ Input.suppress("space","right","left","up","down","home","end","tab");
 
 //Define Inputs
 inputs = {
-   "jump": ["space","w","up","shift","z","x",",","."]
+   "jump": ["space","w","up","shift","z","x",",","."],
+   "continue": ["space","w","up","shift","z","x",",",".","enter","r","lmb"]
 }
