f95758c003897e8d98ca974352e028be3deea408
Ewan Breakey <ebreakey@gmail.com>
2016-06-25 23:21:55 +1000

    Polish on  Score

--- scr/Draw/Distance.js
+++ scr/Draw/Distance.js
@@ -7,17 +7,33 @@ function drawDistance(ctx) {
 
    //Convert Distance to Array of chars
    let dist = (""+ground.distance).split("");
-   for (let i=dist.length;i--;) {
-      let sprite = SL.Sprite("gui.text."+dist[i]);
-      sprite.draw(xx+(i*24),yy,ctx);
-   }
-
-   //Convert Distance to Array of chars
    let bd = +Oven.getCookie("bestDistance") || 0;
    let bdist = (""+bd).split("");
+
+   if (dist.length < bdist.length) {
+      for (let i = dist.length;i<bdist.length;i++)
+         dist.insert("0");
+   }
+
+   if (bdist.length < dist.length) {
+      for (let i = bdist.length;i<dist.length;i++)
+         bdist.insert("0");
+   }
+
+   //Add 'HI' Text
+   bdist.insert("HI");
+
+   for (let i=dist.length;i--;) {
+      let sprite = SL.Sprite("gui.text."+dist[i]);
+      sprite.draw((xx-((dist.length/2)*sep))+(i*sep),yy,ctx);
+   }
+
    for (let i=bdist.length;i--;) {
-      let sprite = SL.Sprite("gui.text."+bdist[i]);
-      sprite.draw(xx+(i*24),yy+24,ctx);
+      if (bdist[i] == "HI")
+         sprite = SL.Sprite("gui.hi");
+      else
+         sprite = SL.Sprite("gui.text."+bdist[i]);
+      sprite.draw((xx-(((dist.length)/2)*sep))+(i*sep) - 25,yy+sep,ctx);
    }
 
 }
